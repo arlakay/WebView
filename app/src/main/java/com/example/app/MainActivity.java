070861195c42755f0c5a338ab82ebcc6c427049c
arlakay <ersarizkidimitri@yahoo.co.id>
2016-09-15 11:02:46 +0700

    1 commit

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.app;
 
 
-
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
 
     @Override
@@ -22,20 +19,17 @@ public class MainActivity extends Activity {
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
 
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new MyAppWebViewClient());
 
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
         // Use remote resource
-        mWebView.loadUrl("http://www.octolink.co.id/octohealth/login.php");
-
+        mWebView.loadUrl("http://popconasia.com/popconizer/");
 
     }
 
-    // Prevent the back-button from closing the app
     @Override
     public void onBackPressed() {
         if(mWebView.canGoBack()) {
@@ -47,23 +41,19 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/example/app/MyAppWebViewClient.java
+++ app/src/main/java/com/example/app/MyAppWebViewClient.java
@@ -9,7 +9,7 @@ public class MyAppWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (Uri.parse(url).getHost().endsWith("http://www.octolink.co.id/")) {
+        if (Uri.parse(url).getHost().endsWith("popconasia.com")) {
             return false;
         }
 
@@ -17,4 +17,12 @@ public class MyAppWebViewClient extends WebViewClient {
         view.getContext().startActivity(intent);
         return true;
     }
+
+    @Override
+    public void onPageFinished(WebView view, String url){
+//        view.loadUrl("javascript:if (typeof(document.getElementsByClassName('td-header-wrap')[0]) != 'undefined' && document.getElementsByClassName('td-header-wrap')[0] != null){document.getElementsByClassName('td-header-wrap')[0].style.display = 'none';} void 0");
+//        view.loadUrl("javascript:if (typeof(document.getElementsByClassName('td-sub-footer-container')[0]) != 'undefined' && document.getElementsByClassName('td-sub-footer-container')[0] != null){document.getElementsByClassName('td-sub-footer-container')[0].style.display = 'none';} void 0");
+//        view.loadUrl("javascript:if (typeof(document.getElementsByClassName('td-main-sidebar')[0]) != 'undefined' && document.getElementsByClassName('td-main-sidebar')[0] != null){document.getElementsByClassName('td-main-sidebar')[0].style.display = 'none';} void 0");
+    }
+
 }
